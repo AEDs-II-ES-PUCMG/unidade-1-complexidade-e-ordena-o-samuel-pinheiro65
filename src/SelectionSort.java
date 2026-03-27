4e1bed0b3381472a9993c626baba63805a203250
samuel-pinheiro65 <samuel.almeidapinheiro@gmail.com>
2026-03-27 20:35:42 -0300

    aula

---
+++ src/SelectionSort.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+public class SelectionSort<T extends Comparable<T>> implements IOrdenador<T> {
+    private int comparacoes;
+    private int movimentacoes;
+    private double tempoOrdenacao;
+    private double inicio;
+
+    private double nanoToMilli = 1.0/1_000_000;
+
+    @Override
+    public int getComparacoes() {
+        return comparacoes;
+    }
+
+    @Override
+    public int getMovimentacoes() {
+        return movimentacoes;
+    }
+
+    @Override
+    public double getTempoOrdenacao() {
+        return tempoOrdenacao;
+    }
+
+    private void iniciar(){
+        this.comparacoes = 0;
+        this.movimentacoes = 0;
+        this.inicio = System.nanoTime();
+    }
+
+    private void terminar(){
+        this.tempoOrdenacao = (System.nanoTime() - this.inicio) * nanoToMilli;
+    }
+
+    private void swap(int x, int y, T[] vetor) {
+        T temp = vetor[x];
+        vetor[x] = vetor[y];
+        vetor[y] = temp;
+        movimentacoes+=3;
+    }
+
+    @Override
+    public T[] ordenar(T[] dados) {
+        T[] dadosOrdenados = Arrays.copyOf(dados, dados.length);
+        int tamanho = dadosOrdenados.length;
+        iniciar();
+        for (int i = 0; i < tamanho - 1; i++) {
+            int menorIndice = i;
+            for (int j = i + 1; j < tamanho; j++) {
+                comparacoes++;
+                if (dadosOrdenados[j].compareTo(dadosOrdenados[menorIndice]) < 0) {
+                    menorIndice = j;
+                }
+            }
+            if (menorIndice != i) {
+                swap(i, menorIndice, dadosOrdenados);
+            }
+        }	
+        terminar();
+        return dadosOrdenados;
+    }
+}
